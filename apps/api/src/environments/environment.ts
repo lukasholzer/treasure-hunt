01494fc582db95182b50607a3123a4de9b8599ba
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-04-26 15:03:12 +0200

    chore: Revert prod mode disabling

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   redis: {
     host: 'localhost',
-    port: 32768,
+    port: 6379,
     // db: parseInt(process.env.REDIS_DB, 10),
     // password: process.env.REDIS_PASSWORD,
     keyPrefix: 'th_',
--- apps/app/src/environments/environment.ts
+++ apps/app/src/environments/environment.ts
@@ -2,26 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   api: {
-//     url: 'http://localhost:3333/',
-//   },
-// };
-
 export const environment = {
   production: false,
   api: {
-    url: 'https://api.treasure-hunt.app/',
+    url: 'http://localhost:3333/',
   },
 };
-
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
